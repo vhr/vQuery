874115e4285692dbc22e9f958e896c80bfbf855c
Valentin Hristov <v.hristov@mail.ru>
2012-03-14 15:01:52 +0200

    Remove old file

--- jquery.textarea.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('textarea[maxlength]').keyup(function(){
-        var max = parseInt($(this).attr('maxlength'));
-        if($(this).val().length > max){
-            $(this).val($(this).val().substr(0, $(this).attr('maxlength')));
-        }
-    });
-});
